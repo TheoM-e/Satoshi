6b902022e383b4a69cbb8a15c9ec1765f03c63bf
Theo Messe <theo.messe@protonmail.com>
2021-12-24 17:02:54 +0100

    add status management

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import time
 import urllib.request
 import discord
 
+client = discord.Client()
 
 # Get discord token from ./key/discord_token.key
 # discord_token.key must only contain your token
@@ -13,4 +14,33 @@ with open('keys/discord_token.key') as file:
 with open('settings/bot_settings.json') as file:
     settings = json.load(file)
 
-client = discord.Client()
+
+@client.event
+async def on_ready():
+    await client.change_presence(
+        activity=discord.Activity(
+            type=get_status(settings['status_type'])[0],
+            name=get_status(settings['status_type'])[1]
+        )
+    )
+
+
+def get_status(type):
+    if 'playing' in type.lower():
+        return [discord.ActivityType.playing, settings['status']]
+    elif 'streaming' in type.lower():
+        return [discord.ActivityType.streaming, settings['status']]
+    elif 'listening' in type.lower():
+        return [discord.ActivityType.listening, settings['status']]
+    elif 'watching' in type.lower():
+        return [discord.ActivityType.watching, settings['status']]
+    else:
+        raise UnknowStatusType("Status type must be: playing/streaming/listening/watching")
+
+
+class UnknowStatusType(Exception):
+    def __init__(self, error):
+        super().__init__(error)
+
+
+client.run(TOKEN)
